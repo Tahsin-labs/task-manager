fa60fb0941861a6830db5bfde41c516043e79574
Tahsin Habib <your-email@example.com>
2026-07-02 19:38:17 +0600

    neno db database connected

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
 const app : Application= express()
 const port = 5000
+
+app.use(express.json()) //-----middleWear
+
+const pool = new  Pool({
+  connectionString : "postgresql://neondb_owner:npg_JjwL3f5XTzCK@ep-fragrant-flower-at4odiol-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
 
 app.get('/', (req :Request, res: Response) => {
   res.status(200).json({
@@ -10,6 +17,15 @@ app.get('/', (req :Request, res: Response) => {
 })
 
 
+app.post('/',async(req:Request,res:Response)=>{
+  //  console.log(req.body)
+
+  const {name, email, password} =req.body;
+  res.status(201).json({
+    message: "created",
+    data:name,email,
+  });
+})
 
 
 
