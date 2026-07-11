feb80f7000e68da7317738cd3a1fa097c045b9a3
Tahsin Habib <your-email@example.com>
2026-07-11 22:53:51 +0600

    logger middlewear added

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,23 @@ import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./auth/auth.route";
+import fs from "fs"
 const app: Application = express()
 const port = config.port;
 
 app.use(express.json()) //-----middleWear
+app.use(express.text()) //-----middleWear
+app.use(express.urlencoded({extended:true})
+) 
+
+app.use((req,res, next)=>{
+  console.log("Method - URL - Time:", req.method, req.url, Date.now());
+  const log = `Method -> ${req.method} Time -> ${Date.now()}URL ->${req.url}`
+  fs.appendFile("logger.txt", log,(err)=>{
+    console.log(err)
+  })
+  next();
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
