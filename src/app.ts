400b143526107861b25f76a93096baafacd41bbd
Tahsin Habib <your-email@example.com>
2026-07-11 23:20:49 +0600

    all user validate by access token

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,15 @@ import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./auth/auth.route";
-import fs from "fs"
+import logger from "./middleware/logger";
+
 const app: Application = express()
 const port = config.port;
 
 app.use(express.json()) //-----middleWear
 app.use(express.text()) //-----middleWear
-app.use(express.urlencoded({extended:true})
-) 
-
-app.use((req,res, next)=>{
-  console.log("Method - URL - Time:", req.method, req.url, Date.now());
-  const log = `Method -> ${req.method} Time -> ${Date.now()}URL ->${req.url}`
-  fs.appendFile("logger.txt", log,(err)=>{
-    console.log(err)
-  })
-  next();
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(logger)
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
@@ -31,7 +23,7 @@ app.get('/', (req: Request, res: Response) => {
 })
 app.use("/api/users", userRoute)
 app.use("/api/profile", profileRoute)
-app.use("/api/auth",authRoute)
+app.use("/api/auth", authRoute)
 
 
 export default app
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,13 +1,14 @@
 import { Router, type Request, type Response } from "express";
 import { pool } from "../../db";
 import { userController } from "./usre.controller";
-
-
+import auth from "../../middleware/auth";
 
 const router = Router()
 
+
+
 router.post('/', userController.createUser)
-router.get("/", userController.getAllUsers)
+router.get("/",auth(), userController.getAllUsers)
 router.get("/:id", userController.getSingleUser)
 router.put("/:id", userController.updateUser)
 router.delete("/:id", userController.deleteUser)
