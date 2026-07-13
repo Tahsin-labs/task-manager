273b139af3675f67f8fd538b35eab7958874f02e
Tahsin Habib <your-email@example.com>
2026-07-13 19:12:15 +0600

    set cors to unknone requst

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,23 @@ import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./auth/auth.route";
 import logger from "./middleware/logger";
-import  CookieParser from "cookie-parser"
+import CookieParser from "cookie-parser"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 const app: Application = express()
 const port = config.port;
+
 
 app.use(cookieParser())
 app.use(express.json()) //-----middleWear
 app.use(express.text()) //-----middleWear
 app.use(express.urlencoded({ extended: true }))
 app.use(logger)
+
+app.use(cors({
+  origin: 'http://localhost:5000',
+
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
